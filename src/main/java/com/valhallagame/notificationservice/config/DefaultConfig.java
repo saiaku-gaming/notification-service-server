54cdd199f98a03afb7991949cce1b81da78bafec
Mio Engel <mioengel@gmail.com>
2018-03-22 17:50:42 +0100

    Added development profile

--- src/main/java/com/valhallagame/notificationservice/config/DefaultConfig.java
+++ src/main/java/com/valhallagame/notificationservice/config/DefaultConfig.java
@@ -4,15 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-import com.valhallagame.common.DefaultServicePortMappings;
 import com.valhallagame.instanceserviceclient.InstanceServiceClient;
 
 @Configuration
-@Profile("development")
-public class DevConfig {
+@Profile("default")
+public class DefaultConfig {
 	@Bean
 	public InstanceServiceClient instanceServiceClient() {
-		InstanceServiceClient.init("http://dev-instance-service:" + DefaultServicePortMappings.INSTANCE_SERVICE_PORT);
 		return InstanceServiceClient.get();
 	}
 }
