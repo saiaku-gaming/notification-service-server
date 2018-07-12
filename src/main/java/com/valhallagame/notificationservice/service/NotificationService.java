e818c888437e2fe3668facb4d1ff82850ac69ab4
Mikael Berglund <mikbe@kth.se>
2018-07-12 20:00:43 +0200

    Fixed crash when message did not have a username, weird stuff.

--- src/main/java/com/valhallagame/notificationservice/service/NotificationService.java
+++ src/main/java/com/valhallagame/notificationservice/service/NotificationService.java
@@ -111,28 +111,28 @@ public class NotificationService {
 
 	public synchronized void addNotification(NotificationData message) {
 		logger.info("Add Notification: {}", message);
-		try {
-			String playerServerLocation = personServerLocations.get(message.getUsername());
+		if(message.getUsername() == null){
+			logger.error("Tried to add a netification without username! " + message);
+			return;
+		}
+		String playerServerLocation = personServerLocations.get(message.getUsername());
 
-			if (playerServerLocation == null) {
-				logger.info("Could not find {} in a server", message.getUsername());
-				message.setRetries(message.getRetries() + 1);
-				unsentNotifications.add(message);
-				return;
-			}
+		if (playerServerLocation == null) {
+			logger.info("Could not find {} in a server", message.getUsername());
+			message.setRetries(message.getRetries() + 1);
+			unsentNotifications.add(message);
+			return;
+		}
 
-			NotificationSender notificationSender = notificationSenders.get(playerServerLocation);
+		NotificationSender notificationSender = notificationSenders.get(playerServerLocation);
 
-			if (notificationSender == null) {
-				logger.error("The server {} does not have a registered listener", playerServerLocation);
-				return;
-			}
+		if (notificationSender == null) {
+			logger.error("The server {} does not have a registered listener", playerServerLocation);
+			return;
+		}
 
-			if (!notificationSender.sendNotification(message)) {
-				notificationSenders.remove(playerServerLocation);
-			}
-		} catch (Exception e){
-			logger.error("Caught exception when adding a new notification", e);
+		if (!notificationSender.sendNotification(message)) {
+			notificationSenders.remove(playerServerLocation);
 		}
 	}
 
