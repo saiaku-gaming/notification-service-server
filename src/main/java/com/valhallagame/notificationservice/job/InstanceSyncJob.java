e607a83db208665fab959eff9249c5dfd217a395
Mio Engel <mioengel@gmail.com>
2019-03-03 19:25:41 +0100

    More MDC fixing

--- src/main/java/com/valhallagame/notificationservice/job/InstanceSyncJob.java
+++ src/main/java/com/valhallagame/notificationservice/job/InstanceSyncJob.java
@@ -1,14 +1,16 @@
 package com.valhallagame.notificationservice.job;
 
-import java.io.IOException;
-
+import com.valhallagame.notificationservice.service.NotificationService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import com.valhallagame.notificationservice.service.NotificationService;
+import java.io.IOException;
+import java.util.UUID;
 
 @Component
 public class InstanceSyncJob {
@@ -18,15 +20,22 @@ public class InstanceSyncJob {
 	@Autowired
 	private NotificationService notificationService;
 
+	@Value("${spring.application.name}")
+	private String appName;
+
 	@Scheduled(fixedRate = 60000, initialDelay = 5000)
 	public void execute() {
+		MDC.put("service_name", appName);
+		MDC.put("request_id", UUID.randomUUID().toString());
+
 		logger.info("Syncing with instance service started...");
 		try {
 			notificationService.syncSendersAndLocations();
 		} catch (IOException e) {
 			logger.error("Could not sync senders and location", e);
+		} finally {
+			logger.info("Syncing with instance service finished");
+			MDC.clear();
 		}
-		logger.info("Syncing with instance service finished");
 	}
-
 }
